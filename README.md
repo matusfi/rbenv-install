dd509a355f94f67708125594185a2b899dbdb4d6
Matus Laslofi <matus.laslofi@gmail.com>
2014-07-25 13:13:15 +0200

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 rbenv-install
 =============
 
-Simple and dumb rbenv/ruby-build installer
+Simple and dumb installer of [rbenv][] and [ruby-build][].
+
+[rbenv]: https://github.com/sstephenson/rbenv
+[ruby-build]: https://github.com/sstephenson/ruby-build
+
+
+How to use
+----------
+
+Something like this?
+
+```
+$ curl https://github.com/matusfi/rbenv-install/rbenv-install.sh.raw | tee .
+```
---
+++ rbenv-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## This script assumes using bash on Debian/Ubuntu
+
+# Install rbenv for current user
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile
+echo 'eval "$(rbenv init -)"' >> ~/.profile
+
+# Install ruby-build
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+# Reload the current shell profile
+. ~/.profile
+
+# Make sure the build-essential pkg is installed
+sudo apt-get install -y build-essential
